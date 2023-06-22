ccafcec4a229c4588aebd6e20eebe62c26e8eeca
苦麒麟 <kuqilin@outlook.com>
2023-06-23 00:17:49 +0800

    Create variate.c

---
+++ docs/C/variate.c
@@ -0,0 +1,16 @@
+//变量
+
+#include<stdio.h>
+
+int main()
+{
+    //变量可以有short, int, long, long long, float, double等类型
+    short num1 = 10;
+    int num2 = 15;
+    long num3 = 65536;
+    long long num4 = 11451400;
+    float num5 = 11.45f;
+    double num6 = 1.91981;
+    printf("%d\n%d\n%d\n%d\n%f\n%f");
+    return 0;
+}
