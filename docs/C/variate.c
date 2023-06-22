4068615d34ecd30181f631f666dee17863020a28
苦麒麟 <kuqilin@outlook.com>
2023-06-23 00:21:32 +0800

    Update variate.c

--- docs/C/variate.c
+++ docs/C/variate.c
@@ -5,12 +5,12 @@
 int main()
 {
     //变量可以有short, int, long, long long, float, double等类型
-    short num1 = 10;
-    int num2 = 15;
-    long num3 = 65536;
-    long long num4 = 11451400;
-    float num5 = 11.45f;
-    double num6 = 1.91981;
-    printf("%d\n%d\n%d\n%d\n%f\n%f");
+    short num1 = 10;                 //定义短整形num1的值为10
+    int num2 = 15;                   //定义整形num2的值为15
+    long num3 = 65536;               //定义长整形num3的值为65536
+    long long num4 = 11451400;       //定义长长整形num4的值为11451400
+    float num5 = 11.45f;             //定义单精度浮点数num5的值为11.45
+    double num6 = 1.91981;           //定义双精度浮点数num6的值为1.191981
+    printf("%d\n%d\n%d\n%d\n%f\n%f");//输出以上变量
     return 0;
 }
